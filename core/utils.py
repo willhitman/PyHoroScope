ff8e7331513df1f2e74e19a39f4ac6a27de4e97c
willhitman <h180128h@hit.ac.zw>
2021-12-20 12:02:45 +0200

    had placed the utils module in the wrong directory there costed me a night

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,7 @@ import requests
 
 from bs4 import BeautifulSoup
 
+
 # getting horoscope by day
 def get_horoscope_by_day(zodiac_sign, day:str):
     if not "-" in day:
@@ -19,3 +20,11 @@ def get_horoscope_by_week(zodiac_sign: int):
     soup = BeautifulSoup(res.content, 'html.parser')
     data = soup.find('div', attrs={'class':'main-horoscope'})
     return data.p.text
+
+#getting horoscope by month
+def get_horoscope_by_month(zodiac_sign:int):
+    res = requests.get(f"https://www.horoscope.com/us/horoscopes/general/horoscope-general-monthly.aspx?sign={zodiac_sign}")
+    soup = BeautifulSoup(res.content, 'html.parser')
+    data = soup.find('div', attrs={'class':'main-horoscope'})
+    return data.p.text
+
